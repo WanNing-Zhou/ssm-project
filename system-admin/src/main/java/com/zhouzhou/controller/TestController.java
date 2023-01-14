8f6ae1230e0c0e6d78787f534113674a5024d36b
WangNing-Zhou <1879154660@qq.com>
2023-01-15 01:32:40 +0800

    前端登录页面搭建

--- system-admin/src/main/java/com/zhouzhou/controller/TestController.java
+++ system-admin/src/main/java/com/zhouzhou/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.zhouzhou.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  */
 @RestController
+
+@CrossOrigin //处理跨域
 public class TestController {
 
-    @GetMapping("/hello")
+    @GetMapping("/test")
     public String test(){
         return "hello";
     }
